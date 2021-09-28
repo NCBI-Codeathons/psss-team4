667decf51ca3b68d944f8c736278c8d028b63440
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2021-09-28 17:26:19 -0400

    Add results bucket

--- tools/sample-elastic-blast-run.sh
+++ tools/sample-elastic-blast-run.sh
@@ -8,6 +8,7 @@ export PATH=/bin:/usr/local/bin:/usr/bin
 set -euo pipefail
 shopt -s nullglob
 
+results_bucket=${1:-"gs://psss-team4/test-results-$USER"}
 export ELB_GCP_PROJECT=codeathon-psss-2021
 export ELB_GCP_REGION=us-east4
 export ELB_GCP_ZONE=us-east4-c
@@ -15,9 +16,9 @@ export ELB_LOGLEVEL=DEBUG
 
 elastic-blast --version
 
-elastic-blast submit --db pdbnt --query gs://elastic-blast-samples/queries/MANE/MANE.GRCh38.v0.8.select_refseq_rna.fna --program blastn \
+elastic-blast submit --db pdbnt --query gs://elastic-blast-samples/queries/MANE/MANE.GRCh38.v0.8.select_refseq_rna.fna --program blastn --results ${results_bucket} \
     --num-nodes 1 \
-    --instance-type n1-standard-32 \
+    --machine-type n1-standard-32 \
     --num-cpus 2 \
     --mem-limit 1G \
     -- -outfmt '7 std staxids'
