e6c672e4e36dddee8ed7600ee23c04eea7283e7f
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2021-09-29 09:01:52 -0400

    Add comments

--- tools/sample-elastic-blast-run.sh
+++ tools/sample-elastic-blast-run.sh
@@ -7,7 +7,9 @@
 set -euo pipefail
 shopt -s nullglob
 
+# Documentation on results: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#results
 results_bucket=${1:-"gs://psss-team4/test-results-$USER"}
+
 export ELB_GCP_PROJECT=codeathon-psss-2021
 export ELB_GCP_REGION=us-east4
 export ELB_GCP_ZONE=us-east4-c
@@ -21,22 +23,39 @@ trap " /bin/rm -fr $CFG " INT QUIT EXIT HUP KILL ALRM
 
 cat >$CFG <<EOF
 [cloud-provider]
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#gcp-project
 gcp-project = $ELB_GCP_PROJECT
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#gcp-region
 gcp-region = $ELB_GCP_REGION
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#gcp-zone
 gcp-zone = $ELB_GCP_ZONE
+
+# These two settings are needed because the GCP project doesn't have a 'default' network
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#gcp-network
 gcp-network = research
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#gcp-sub-network
 gcp-subnetwork = subnet-us-east4
 
 [cluster]
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#machine-type
 machine-type = n1-standard-32
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#number-of-worker-nodes
 num-nodes = 1
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#number-of-cpus
 num-cpus = 2
 
 [blast]
+# See documentation for supported programs: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#blast-program
 program = blastn
+# See documentation for using your own BLASTDB:
+# https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#blast-database
+# https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/tutorials/create-blastdb-metadata.html?highlight=create
 db = pdbnt
+# See documentation for supported formats: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#query-sequence-data
 queries = gs://elastic-blast-samples/queries/MANE/MANE.GRCh38.v0.8.select_refseq_rna.fna
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#memory-limit-for-blast-search 
 mem-limit = 1G
+# Documentation: https://blast.ncbi.nlm.nih.gov/doc/elastic-blast/configuration.html#blast-options
 options = -outfmt '7 std staxids'
 EOF
 
