69b8883464ffc6bbda4fde7cf8eb4552d0012e1b
victorlin <13424970+victorlin@users.noreply.github.com>
2021-10-01 18:45:19 +0000

    fix format string

--- tools/get-fasta/main.py
+++ tools/get-fasta/main.py
@@ -13,7 +13,7 @@ def get_fasta(request):
     bucket = client.get_bucket(bucket_name)
     tmp_dir = TemporaryDirectory()
     subprocess.call(f"./get-fasta.sh {tmp_dir.name} {run_accession}", shell=True, executable='/bin/bash')
-    fasta_file = '{run_accession}.fasta'
+    fasta_file = f'{run_accession}.fasta'
     blob = bucket.blob(f'{upload_prefix}{fasta_file}')
     blob.upload_from_filename(filename=os.path.join(tmp_dir.name, 'tmp.fasta'))
     return f'gs://{bucket_name}/{upload_prefix}{fasta_file}'
