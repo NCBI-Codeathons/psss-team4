dff82ef131615eed07a5c49412b695b71f892980
victorlin <13424970+victorlin@users.noreply.github.com>
2021-10-01 01:30:22 +0000

    cleanup

--- tools/get-fasta/main.py
+++ tools/get-fasta/main.py
@@ -8,23 +8,10 @@ client = storage.Client()
 bucket = client.get_bucket(bucket_name)
 
 def get_fasta(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.make_response>`.
-    """
     request_json = request.get_json()
-
     run_accession = request_json['run_accession']
-    try:
-        tmp_dir = TemporaryDirectory()
-        subprocess.call(f"./get-fasta.sh {tmp_dir.name} {run_accession}", shell=True, executable='/bin/bash')
-    except Exception as e:
-        return str(e)
-
+    tmp_dir = TemporaryDirectory()
+    subprocess.call(f"./get-fasta.sh {tmp_dir.name} {run_accession}", shell=True, executable='/bin/bash')
     blob = bucket.blob(f'fasta/{run_accession}.fasta')
     blob.upload_from_filename(filename=os.path.join(tmp_dir.name, 'tmp.fasta'))
     return f'Uploaded {bucket_name}/fasta/{run_accession}.fasta'
