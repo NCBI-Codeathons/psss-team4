83a45cf48083c734e4ea56ae9c255d14c4adf992
Victor Lin <13424970+victorlin@users.noreply.github.com>
2021-10-01 11:46:23 -0700

    Add upload params, remove environment variables (#11)

--- tools/get-fasta/main.py
+++ tools/get-fasta/main.py
@@ -3,15 +3,17 @@ import subprocess
 from tempfile import TemporaryDirectory
 from google.cloud import storage
 
-bucket_name = os.getenv('BUCKET')
 client = storage.Client()
-bucket = client.get_bucket(bucket_name)
 
 def get_fasta(request):
     request_json = request.get_json()
     run_accession = request_json['run_accession']
+    bucket_name = request_json['upload_bucket']
+    upload_prefix = request_json['upload_prefix']
+    bucket = client.get_bucket(bucket_name)
     tmp_dir = TemporaryDirectory()
     subprocess.call(f"./get-fasta.sh {tmp_dir.name} {run_accession}", shell=True, executable='/bin/bash')
-    blob = bucket.blob(f'fasta/{run_accession}.fasta')
+    fasta_file = f'{run_accession}.fasta'
+    blob = bucket.blob(f'{upload_prefix}{fasta_file}')
     blob.upload_from_filename(filename=os.path.join(tmp_dir.name, 'tmp.fasta'))
-    return f'Uploaded {bucket_name}/fasta/{run_accession}.fasta'
+    return f'gs://{bucket_name}/{upload_prefix}{fasta_file}'
